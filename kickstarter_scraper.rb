94a493d3056f73f7f69e28689ae54e3679a539aa
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-20 02:54:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -25,5 +25,5 @@ def create_project_hash
       location: project.css("ul.project-meta span.location-name").text,
       percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-  end 
+  end
 end
